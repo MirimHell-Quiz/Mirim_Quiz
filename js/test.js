8b1137a2a1d19207f2c4225a4bb6e09fd1f5ab26
Subin <w2208hd@gmail.com>
2024-05-24 13:51:13 +0900

    <🐛> [fix] : 엔터 클릭하고 10초 후 div 안 뜨는 버그 고침

--- js/test.js
+++ js/test.js
@@ -2,12 +2,15 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 let id = parseInt(urlParams.get('id')); // Convert ID to integer
 
+// 버튼 클릭 시간을 기록할 변수
+let isButtonClicked = false;
+
 //id 를 기준으로 퀴즈를 차례대로 불러옴
 function loadQuiz(id) {
     buttonClicked = false;
+    isButtonClicked = false; // 새로운 문제를 로드할 때 변수 초기화
     const quiz = Quiz_date.find(quiz => quiz.id === id);
     //Quiz_date배열에서 있는 id가 지금 받은 아이디와 일치하냐 체크
-
 
     //맞다면
     if (quiz) {
@@ -32,7 +35,7 @@ function loadQuiz(id) {
     } else {
         console.error('Quiz not found for ID:', id);
     }
-    setTimeout(checkTimeout, 8000);
+    setTimeout(checkTimeout, 10000);
 }
 
 loadQuiz(id); // Initial load with the received ID
@@ -48,10 +51,6 @@ for (let i = 0; i < 4; i++) {
     document.querySelector(`.button${i + 1}`).addEventListener('click', handleButtonClick2);
 }
 
-
-// 버튼 클릭 시간을 기록할 변수
-let isButtonClicked = false;
-
 // 버튼 클릭 이벤트 처리 함수
 function handleButtonClick2() {
     // 버튼이 클릭되었음을 표시
@@ -65,7 +64,7 @@ function checkTimeout() {
     // 버튼이 클릭되지 않았을 때 경고 창에 '2' 텍스트를 표시
     if (!isButtonClicked) {
         const warningText = document.querySelector('.warning-window-div-2 p');
-        warningText.textContent = 'sdfsdf';
+        warningText.textContent = '2';
         showWarningWindow();
     }
 }
@@ -82,7 +81,6 @@ function showWarningWindow() {
         const warningText = document.querySelector('.warning-window-div-2 p');
         warningText.textContent = '1';
     }
-    
 }
 
 // 버튼 클릭 이벤트 리스너 등록
@@ -95,16 +93,15 @@ buttons.forEach(button => {
 document.addEventListener('keydown', function(event) {
     let eventDiv = document.querySelector('.warning-window-div-1');
     if (event.key === 'Enter') {
-        if (eventDiv .style.display == 'block') {
+        if (eventDiv.style.display == 'block') {
             console.log("!!");
-            eventDiv .style.display = 'none';
+            eventDiv.style.display = 'none';
             document.querySelector('.container').style.filter = 'none';
             event.preventDefault();
             handleButtonClick();
         }
     }
 });
-
 
 // 모든 버튼에 클릭 이벤트 리스너 추가
 document.querySelectorAll('button').forEach(function(button) {
@@ -113,5 +110,3 @@ document.querySelectorAll('button').forEach(function(button) {
         document.querySelector('.container').style.filter = 'blur(5px)';
     });
 });
-
-
