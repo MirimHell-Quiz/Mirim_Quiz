d41c135dff4ee595fd041ce915b38348348d0e03
arin0220 <w2233@e-mirim.hs.kr>
2024-06-16 11:37:00 +0900

    <🐛> [fix] : 로그인 회원가입 버그 수정

--- php/SignUp.php
+++ php/SignUp.php
@@ -5,6 +5,13 @@ $conn = mysqli_connect('localhost', 'test', '1111', 'testdb', 3307);
 // 사용자 입력 값 가져오기
 $id = mysqli_real_escape_string($conn, $_POST['user_id']);
 
+// ID 중복 검사 함수
+function isIdAvailable($conn, $id) {
+    $sql = "SELECT * FROM User WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    return mysqli_num_rows($result) == 0; // 중복되지 않으면 true 반환
+}
+
 // API에서 닉네임 가져오기
 function getNickname() {
     $url = 'https://www.rivestsoft.com/nickname/getRandomNickname.ajax';
@@ -35,26 +42,32 @@ function isNicknameAvailable($conn, $nickname) {
     return mysqli_num_rows($result) == 0; // 중복되지 않으면 true 반환
 }
 
-// 닉네임 생성 및 중복 검사
-$nickname = getNickname();
-while (!isNicknameAvailable($conn, $nickname)) {
+// ID 중복 검사
+if (isIdAvailable($conn, $id)) {
+    // 닉네임 생성 및 중복 검사
     $nickname = getNickname();
-}
+    while (!isNicknameAvailable($conn, $nickname)) {
+        $nickname = getNickname();
+    }
 
-// SQL 쿼리 생성
-$sql = "INSERT INTO User (id, Nickname) VALUES ('$id', '$nickname')"; 
+    // SQL 쿼리 생성
+    $sql = "INSERT INTO User (id, Nickname) VALUES ('$id', '$nickname')"; 
 
-// 쿼리 실행
-if (mysqli_query($conn, $sql)) {
-    // 삽입된 직후의 StudentKey 가져오기
-    $studentKey = mysqli_insert_id($conn);
-    
-    // 회원가입 성공 시 menu.html 페이지로 이동
-    echo "<script>alert('회원가입이 완료되었습니다.');</script>";
-    echo "<script>window.location.href = '../html/menu.html?id=$studentKey';</script>";
+    // 쿼리 실행
+    if (mysqli_query($conn, $sql)) {
+        // 삽입된 직후의 StudentKey 가져오기
+        $studentKey = mysqli_insert_id($conn);
+        
+        // 회원가입 성공 시 menu.html 페이지로 이동
+        echo "<script>alert('회원가입이 완료되었습니다.');</script>";
+        echo "<script>window.location.href = '../html/menu.html?id=$studentKey';</script>";
+    } else {
+        // 회원가입 실패 시 실패 이유를 알림창에 표시
+        echo "<script>alert('회원가입에 실패했습니다. 오류: " . mysqli_error($conn) . "');</script>";
+    }
 } else {
-    // 회원가입 실패 시 실패 이유를 알림창에 표시
-    echo "<script>alert('회원가입에 실패했습니다. 오류: " . mysqli_error($conn) . "');</script>";
+    // ID가 중복된 경우 알림창에 표시
+    echo "<script>alert('회원가입에 실패했습니다. 오류: 중복된 ID입니다.');</script>";
 }
 
 // 데이터베이스 연결 해제
