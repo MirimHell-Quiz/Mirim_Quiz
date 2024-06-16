f2c82252aea5181e6a8450f61e5f8eeb6a47026a
w2208hd <w2208hd@gmail.com>
2024-06-16 17:55:27 +0900

    <💄> [design] : 전체화면일 때 css 수정, index 애니메이션 수정

--- js/Start.js
+++ js/Start.js
@@ -7,12 +7,13 @@ function getStudentKey() {
 // id 값을 추출
 const studentKey = getStudentKey();
 
-document.querySelector('.start').addEventListener('click', function() {
-// ID 1로 퀴즈 시작 페이지로 이동
+document.querySelector('.start').addEventListener('click', function () {
+    // ID 1로 퀴즈 시작 페이지로 이동
     window.location.href = `../php/remember_name.php?id=${studentKey}`;
 });
 
-document.querySelector('.back').addEventListener('click', function() {
-// 취소 버튼 클릭 시, 사용자 정의 작업 수행 (예: 다른 페이지로 이동)
+document.querySelector('.back').addEventListener('click', function () {
+    // 취소 버튼 클릭 시, 사용자 정의 작업 수행 (예: 다른 페이지로 이동)
     alert('퀴즈를 취소하셨습니다.');
+    window.location.href = `../index.html`;
 });
