424352419b5f4a73ddaa8e154135be8c870153c2
w2208hd <w2208hd@gmail.com>
2024-06-16 17:19:45 +0900

    <🐛> [fix] : 퀴즈 부분 이상하게 되는 버그 고침침

--- js/countdown.js
+++ js/countdown.js
@@ -2,20 +2,34 @@ const progressBarElem = document.querySelector('.progress-bar__bar');
 const audio = document.getElementById('audio_play');
 let isAudioPlayed = false;
 let countdownTimeout;
+let countdownInterval;
 
 // 카운트다운 시작 함수
 function startCountdown() {
+    stopCountdown(); // 기존 카운트다운 정지
+    let timeLeft = 9; // 9초
+    progressBarElem.classList.add('active');
+
+    countdownInterval = setInterval(() => {
+        timeLeft--;
+        if (timeLeft <= 0) {
+            clearInterval(countdownInterval);
+            noClickButton();
+        }
+    }, 1000);
+
     countdownTimeout = setTimeout(noClickButton, 9000); // 9초 후에 noClickButton 함수 호출
 }
 
 // 카운트다운 정지 함수
 function stopCountdown() {
     clearTimeout(countdownTimeout);
+    clearInterval(countdownInterval);
+    progressBarElem.classList.remove('active');
 }
 
 // 자동 시작 함수
 function autoStart() {
-    progressBarElem.classList.add('active');
     if (!isAudioPlayed) {
         audio.play();
         isAudioPlayed = true;
@@ -25,23 +39,15 @@ function autoStart() {
 
 // 카운트다운과 오디오 정지 함수
 function stopCountdownAndAudio() {
-    progressBarElem.classList.remove('active');
-    clearTimeout(countdownTimeout);
-
+    stopCountdown();
     audio.pause();
     audio.currentTime = 0;
 }
 
 // 카운트다운과 오디오 재설정 함수
 function resetCountdownAndAudio() {
-    progressBarElem.classList.remove('active');
-    clearTimeout(countdownTimeout);
-
-    audio.pause();
-    audio.currentTime = 0;
-
+    stopCountdownAndAudio();
     audio.play();
-
     setTimeout(autoStart, 10); // 10ms 후에 다시 시작하도록 설정
 }
 
@@ -52,7 +58,7 @@ function showContainerDiv() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(autoStart, 0);
+    autoStart();
 
     const buttons = document.querySelectorAll('.container-button button');
     buttons.forEach(button => {
@@ -62,8 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // .warning-window-div-1 클릭 이벤트 추가
-    document.querySelector('.warning-window-div-1').addEventListener('click', (event) => {
+    document.querySelector('.warning-window-div-1').addEventListener('click', () => {
         resetCountdownAndAudio();
     });
 });
